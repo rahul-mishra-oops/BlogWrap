923ed6b7f871edbb31a98b81881efcb9767d1973
Rahul Jyoti Mishra <rahuljyotimishra2002@gmail.com>
2023-07-21 16:42:09 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { logo } from '../assets'
+
+const Hero = () => {
+    return (
+        <header className='w-full flex justify-center items-center flex-col'>
+            <nav className="flex justify-between items-center flex-row w-full mb-10 pt-3">
+                <button type='button' onClick={() => window.open('https://github.com/rahul-mishra-oops/BlogWrap.git')} className='black_btn'>
+                    Github
+                </button>
+            </nav>
+            <h1 className='head_text'>
+                Summarize articles with <br className='max-md:hidden'/>
+                <span className='orange_gradient'>OpenAI GPT-4</span>
+            </h1>
+            <h2 className='desc'> Summarize your article with BlogWrap. Get an AI generated summary to a lengthy wholesome article. </h2>
+        </header>
+    )
+}
+
+export default Hero
